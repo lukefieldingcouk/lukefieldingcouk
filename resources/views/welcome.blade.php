58de80e35b3bf823f0f5c23baaf6d70e58bb6f6e
lukefieldingcouk <github@lukefielding.co.uk>
2023-11-19 12:20:27 +0000

    Added node_modules and Tailwind CSS

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,8 +9,8 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <!-- Compiled Tailwind CSS -->
+        @vite('public/css/app.css')
     </head>
 
     <body class="antialiased">
